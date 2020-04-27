c3b9e554d8ec5dcaf78cca2028079ffe4f5c19b4
Jordan Rodriguez <a@jordanrodriguez.net>
2020-04-26 23:13:20 -0700

    batters.html: Fixed step3 being active at all times updater.css: Attempt to make checkboxes bigger failed getUpdate.js: Attempt 1 at creating the block of code for updates

--- js/getUpdate.js
+++ js/getUpdate.js
@@ -65,4 +65,49 @@ function getUpdate(){
     else{
         $("#outputTotal").html("Total TPE: " + oldTotalTPE + " -> " + outputTotal);
     }
+    
+    var codeBlock = "";
+    var weeklyTrainingCheckbox = document.getElementById('WTCheck').checked;
+    var activityCheckCheckbox = document.getElementById('ACCheck').checked;
+    var pointTaskCheckbox = document.getElementById('PTCheck').checked;
+    var journalPointTaskCheckbox = document.getElementById('JPTCheck').checked;
+    var pictureChainCheckbox = document.getElementById('PCCheck').checked;
+    var primetimeCheckbox = document.getElementById('PrimetimeCheck').checked;
+    var other1Checkbox = document.getElementById('other1Check').checked;
+    var other2Checkbox = document.getElementById('other2Check').checked;
+    var other3Checkbox = document.getElementById('other3Check').checked;
+    var other4Checkbox = document.getElementById('other4Check').checked;
+    
+    if(weeklyTrainingCheckbox == true){
+        codeBlock += "URL=" + $("#WTLink").val() + "]WT #" + $("#WTNumber").val() + "[/URL] (+" + $("#WTTPE").val() + " TPE)\n";
+    }
+    if(activityCheckCheckbox == true){
+        codeBlock += "URL=" + $("#ACLink").val() + "]AC #" + $("#ACNumber").val() + "[/URL] (+" + $("#ACTPE").val() + " TPE)\n";
+    }
+    if(pointTaskCheckbox == true){
+        codeBlock += "URL=" + $("#PTLink").val() + "]PT " + $("#PTNumber").val() + "-" + $("#PTWeek").val() + "[/URL] (+" + $("#PTTPE").val() + " TPE)\n";
+    }
+    if(journalPointTaskCheckbox == true){
+        codeBlock += "URL=" + $("#JPTLink").val() + "]PT " + $("#JPTNumber").val() + "-" + $("#JPTWeek").val() + "[/URL] (+" + $("#JPTTPE").val() + " TPE)\n";
+    }
+    if(pictureChainCheckbox == true){
+        codeBlock += "URL=" + $("#PCLink").val() + "]PT " + $("#PCNumber").val() + "-" + $("#PCWeek").val() + "[/URL] (+" + $("#PCTPE").val() + " TPE)\n";
+    }
+    if(primetimeCheckbox == true){
+        codeBlock += "URL=" + $("#PrimetimeLink").val() + "]PT " + $("#PrimetimeNumber").val() + "-" + $("#PrimetimeWeek").val() + "[/URL] (+" + $("#PrimetimeTPE").val() + " TPE)\n";
+    }
+    if(other1Checkbox == true){
+        codeBlock += "URL=" + $("#other1Link").val() + "]" + $("#Other1Name").val() + "[/URL] (+" + $("#Other1TPE").val() + " TPE)\n";
+    }
+    if(other2Checkbox == true){
+        codeBlock += "URL=" + $("#other2Link").val() + "]" + $("#Other2Name").val() + "[/URL] (+" + $("#Other2TPE").val() + " TPE)\n";
+    }
+    if(other3Checkbox == true){
+        codeBlock += "URL=" + $("#other3Link").val() + "]" + $("#Other3Name").val() + "[/URL] (+" + $("#Other3TPE").val() + " TPE)\n";
+    }
+    if(other4Checkbox == true){
+        codeBlock += "URL=" + $("#other4Link").val() + "]" + $("#Other4Name").val() + "[/URL] (+" + $("#Other4TPE").val() + " TPE)\n";
+    }
+            
+    $("#outCode").html(codeBlock);
 }
