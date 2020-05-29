60f92d0d0ba60fcab0671486c0b05469b190736c
Jordan Rodriguez <a@jordanrodriguez.net>
2020-05-28 21:32:55 -0700

    Update batters.js
    Attempt 2

--- js/batters.js
+++ js/batters.js
@@ -67,7 +67,7 @@ function minMax(str){
 	console.log(indices);
 	var mins = [];
 	for(var uses = 0 ; uses++ ; uses = indices.length){
-		var number = parseInt(str.substring(indices[uses],7));
+		var number = parseInt(str.substring(indices[uses],indices[uses]+7));
 		mins.push(number);
 	}
 	
