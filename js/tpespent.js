bf417f3bd84f313f571c9cb8a731b638e7a2ba02
Jordan Rodriguez <dorzder36@yahoo.com>
2022-10-15 14:31:43 -0700

    Update tpespent.js
    trial #a lot

--- js/tpespent.js
+++ js/tpespent.js
@@ -40,8 +40,8 @@ function updateTPESpent(){
         var newStat = "#newStat" + i;
         var tpeSpent = "#tpeSpent" + i;
 		var statName = "#statName" + i;
-        if($("td:contains('GB')")){
-			tpeSpentValue = 25 * parseInt($(stat).html()) - parseInt($(minStat).html());
+        if($(statName).html() === "*GB*"){
+			tpeSpentValue = 25 * (parseInt($(stat).html()) - parseInt($(minStat).html()));
 		}		
 		else if(parseInt($(stat).html())!== 0 && $(stat).html().length<=3){
             if(parseInt($(minStat).html())<=40){
