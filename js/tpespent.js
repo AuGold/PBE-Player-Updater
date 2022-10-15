02442b93481c71c5dbf0d9ddce98e27325b8bb3a
Jordan Rodriguez <dorzder36@yahoo.com>
2022-10-15 14:20:34 -0700

    Update tpespent.js
    fix )

--- js/tpespent.js
+++ js/tpespent.js
@@ -40,7 +40,7 @@ function updateTPESpent(){
         var newStat = "#newStat" + i;
         var tpeSpent = "#tpeSpent" + i;
 		var statName = "#statName" + i;
-        if($(statName:contains("GB"){
+        if($(statName:contains("GB")){
 			tpeSpentValue = 25 * parseInt($(stat).html()) - parseInt($minStat.html());
 		}		
 		else if(parseInt($(stat).html())!== 0 && $(stat).html().length<=3){
