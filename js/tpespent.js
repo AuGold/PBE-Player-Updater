c2d4fc397bbcb20405ddd253319bb74c64626ca2
Jordan Rodriguez <dorzder36@yahoo.com>
2022-10-15 15:09:19 -0700

    Update tpespent.js
    Fix for pitch mins?

--- js/tpespent.js
+++ js/tpespent.js
@@ -301,8 +301,9 @@ function updateTPESpent(){
 		}else if($(stat).html() == 0){
 			tpeSpentValue = 0;
             if(parseInt($(minStat).html())<=40){
-				if(parseInt($(newStat).val()) == 40){
+				if(parseInt($(newStat).val()) <= 40){
 					tpeSpendingValue = 50;
+					tpeSpendingValue += update1to40 * (parseInt($(newStat).val()) - parseInt($(minStat).html()));
 				}
                 if(parseInt($(newStat).val())<=50 && parseInt($(newStat).val())>40){
                     tpeSpendingValue = 50
