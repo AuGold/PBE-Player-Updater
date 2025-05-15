172c81be83e7c443dbdd53904387af8772fa42f1
Jordan Rodriguez <general@jordanrodriguez.net>
2025-05-15 10:26:17 -0700

    It was good, nevermind

--- js/getUpdate.js
+++ js/getUpdate.js
@@ -5,11 +5,11 @@ function addHighlight(username, url) {
     var h1 = ""
     //hl = `#:~:text=${username.replaceAll(" ", "%20")}`;
     if(username!="nopity nope"){
-	alert(url)
-	//hl = `#:~:text=${username.replaceAll(" ", "%20")}`;
-	//url = url.concat(h1)
-	url += username
-	alert(url)
+	//alert(url)
+	hl = `#:~:text=${username.replaceAll(" ", "%20")}`;
+	url += h1
+	//url += username
+	//alert(url)
     }
     return url;
 }
